f18735fe97325f008f6179aa2820e9f1266f5a73
rebeca2020 <61558667+rebeca2020@users.noreply.github.com>
2022-01-03 15:32:58 -0300

    Exercício modulo 9 da ebac.

---
+++ Exercicio_mod9.java
@@ -0,0 +1,17 @@
+package br.com.exercicio;
+
+//LER UM VALOR NUMÉRICO 
+//COLOCAR UMA VARIÁVEL DO TIPO PRIMITIVA PARA O TIPO WRAPPER
+//CONVERTER PARA O TIPO WRAPPER
+public class Exercicio_mod9 {
+
+	public static void main(String[] args) {
+		
+		int valor = 7;
+		System.out.println(valor);
+		
+		Integer valor2 = valor;  //Pego a primeira variável primitira e adiciona dentro de um objeto wrapper.
+		System.out.println(valor2);
+	}
+
+}
